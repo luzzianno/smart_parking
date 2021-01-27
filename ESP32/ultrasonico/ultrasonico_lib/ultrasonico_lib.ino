eeab3f3e60c802259214f45eb2df64c68f4c02df
miiguii <lucianorichard0@gmail.com>
2021-01-27 16:38:11 -0300

    ejemplos

---
+++ ESP32/ultrasonico/ultrasonico_lib/ultrasonico_lib.ino
@@ -0,0 +1,42 @@
+#include <NewPing.h>
+#include <Wire.h>   // incluye libreria bus I2C
+#include <MechaQMC5883.h> // incluye libreria para magnetometro QMC5883L
+#include "heltec.h"
+
+#define BAND    915E6  //Banda red LoRa
+#define TRIGGER_PIN 12
+#define ECHO_PIN 13
+#define MAX_DISTANCE 200
+
+MechaQMC5883 qmc;
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+void setup() {
+  Wire.begin();  // inicializa bus I2C
+  Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.Heltec.Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
+  Serial.begin(9600);
+  qmc.init();  //inicializa objeto
+
+}
+
+void loop() {
+  int x,y,z,acimut;   // variables para los 3 ejes y acimut
+  qmc.read(&x,&y,&z,&acimut); // funcion para leer valores y asignar a variables
+  
+  Serial.print("Ping: ");
+  Serial.print(sonar.ping_cm());
+  Serial.println("cm");
+  Serial.print("x:");   // muestra texto x:
+  Serial.print(x);    // muestra valor de variable x
+  Serial.print("\t");   // espacio de tabulacion
+  Serial.print("y:");   // muestra y:
+  Serial.print(y);    // muestra valor de variable y
+  Serial.print("\t");   // espacio de tabulacion
+  Serial.print("z:");   // muestra texto z:
+  Serial.print(z);    // muestra valor de variable z
+  Serial.print("\t");   // espacio de tabulacion
+  Serial.print("a:");   // muestra texto a:
+  Serial.println(acimut); // muestra valor de variable acimut
+  delay(250);
+
+}
